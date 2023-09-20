1cfd989a74cfb2ba01d6360a02f973e87bcfd54d
Andre <andre.ranulfo@goodfolk.agency>
2023-09-20 14:09:41 -0300

    👨‍🏫Class 8

--- app/Http/Controllers/Main.php
+++ app/Http/Controllers/Main.php
@@ -11,4 +11,20 @@ class Main extends Controller
     {
        echo "j";
     }
+    public function printName()
+    {
+        return view("page2", [
+            "name" => "This is a page",
+            "lorem" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.",
+            "pgnum" => "2"
+        ]);
+    }
+    public function echoName()
+    {
+        return view("pageb", [
+            "name" => "This is a page",
+            "lorem" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.",
+            "pgnum" => "b"
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,18 @@ Route::get('/', function () {
 
 Route::get("/main", "Main@index");
 
+Route::view("/page", "page" , [
+    "name" => "This is a page",
+    "lorem" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam."
+]
+);
+//Above, we are passing data to the view page
+
+//Below, we are passing data to the controller // It's cleaner to pass data to the controller
+Route::get("/page2", "Main@printName"); // Main is the controller and printName is the function in the controller
+
+Route::get("/pageb", "Main@echoName");
+
 
 
 
